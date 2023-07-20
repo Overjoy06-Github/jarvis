c2a934faaa66db57b509d09d472900e3decc5648
Overjoy06-Github <56468229+Overjoy06-Github@users.noreply.github.com>
2023-07-20 13:28:00 +0800

    Update anime_adventures.py

--- anime_adventures.py
+++ anime_adventures.py
@@ -2,48 +2,64 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-character = "Metal_Knight"
+character = "Heathcliff".title()
 html_text = requests.get(f'https://animeadventures.fandom.com/wiki/{character}').text
-soup = BeautifulSoup(html_text, 'lxml')
-character_needed = soup.find('strong', class_ = 'mw-selflink selflink').text #prints the character
-character_evolution_name = soup.find('div', class_ = 'mobile-hidden').text # prints Guts (Berserk)
-description = soup.find('div', class_ = 'mw-parser-output').find('p').text
-description = description.replace("MythicalMythical", "Mythical")
-banner_raw = soup.find('figure', class_ = 'pi-item pi-image').find('img').get('src') # prints image link
-imgs = soup.findAll('div', class_ = 'mw-parser-output')
+soup = BeautifulSoup(html_text, 'html.parser')
 
-images = []
-for div in imgs:
-    img_tags = div.findAll('img')
-    for img in img_tags:
-        src = img['src']
-        if 'static' in src:
-            images.append(src)
-# 0 - egg of king
-# 1 - non-shiny of unit
-# 2 - shiny of unit
-# 3 - 1st element of unit (physical/magic/true)
-# 4 - 2nd element of unit (fire, wind, etc.)
+#character name | Metal Knight (Bofoi)
+character_name = soup.find('h1', class_ = 'page-header__title')
+character_name = ' '.join(character_name.stripped_strings)
 
-evolution_materials = soup.find('table', class_='article-table game-font-face').text
-split_text = re.split(r"(x\d+)\s*", evolution_materials)
-transformed_text = ' '.join(split_text)
-final = transformed_text.split()
-final = final[:-5]
+#character evolution name | Metal Knight (Arsenal)
+character_div = soup.find('div', class_ = 'mobile-hidden')
+character_evolution_name = character_div.text.strip()
 
-def listToString(s):
-    str1 = " ".join(s)
-    return str1
+#description | Metal Knight  is a Mythical unit based on Metal Knight, from the anime One Punch Man. He is only obtainable through summons, and requires 5k kills to evolve.
+description = soup.find('div', class_ = 'mw-parser-output').find('p')
+extracted_text = soup.find('p').text.strip()
+modified_text = extracted_text.replace("MythicalMythical", "Mythical")
 
-actual_final = listToString(final)
+# images | non_shiny_image | shiny_image
+figures = soup.select('section.pi-item.pi-panel.pi-border-color.wds-tabber figure')
+non_shiny_image = None
+shiny_image = None
+for i, figure in enumerate(figures):
+    image_url = figure.find('img', class_='pi-image-thumbnail')['src']
+    
+    if i == 0:
+        non_shiny_image = image_url
+    elif i == 1:
+        shiny_image = image_url
 
-actual_final = listToString(final)
-actual_final = re.sub(r"\bStar Fruit \(Blue\)(?!\S)", r"Star Fruit <:Star_Fruit_Blue:1131211288454905956>", actual_final)
-actual_final = re.sub(r"\bStar Fruit \(Green\)(?!\S)", r"Star Fruit (Green) <:Star_Fruit_Green:1131211294058496081>", actual_final)
-actual_final = re.sub(r"\bStar Fruit \(Pink\)(?!\S)", r"Star Fruit (Pink) <:Star_Fruit_Pink:1131211253486989413>", actual_final)
-actual_final = re.sub(r"\bStar Fruit \(Rainbow\)(?!\S)", r"Star Fruit (Rainbow) <:Star_Fruit_Rainbow:1131211258696319016>", actual_final)
-actual_final = re.sub(r"\bStar Fruit \(Red\)(?!\S)", r"Star Fruit (Red) <:Star_Fruit_Red:1131211261602955294>", actual_final)
 
-actual_final = actual_final.replace("x", "\nx")
-print(actual_final)
-print(character_evolution_name)
+# Captions for different versions | caption1 - normal | caption2 - shiny
+captions = soup.find_all('figcaption', class_='pi-item-spacing pi-caption')
+caption1 = captions[0].get_text(strip=True)
+caption2 = captions[1].get_text(strip=True)
+
+print(caption1)
+print(caption2)
+'''formatted_materials_str
+# 1 Metal Knight
+35 Full Power Core
+12 Star Fruit
+4 Star Fruit (Blue)
+3 Star Fruit (Pink)
+4 Star Fruit (Green)
+1 Star Fruit (Rainbow)
+'''
+required_items_row = soup.select('table.article-table tr')[0]
+td_elements = required_items_row.find_all('td')
+materials = td_elements[1].get_text(strip=False).replace('Required Items:', '').replace('\n', '').split('x')
+materials = [material.strip() for material in materials if material.strip()]
+def format_material(material):
+    return f"{material[0]}x {material[1]}"
+
+formatted_materials = [format_material(material.split(maxsplit=1)) for material in materials]
+formatted_materials_str = '\n'.join(formatted_materials)
+
+
+
+# Extract and download the images
+image_elements = soup.find_all('img')
+image_urls = [image['src'] for image in image_elements]
